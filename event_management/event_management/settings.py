80977f76e61c77a7e063e80c332ceb049c4503c0
midex-hub <filaniisacc62@gmail.com>
2026-07-08 23:10:11 +0100

    switch back to Gmail SMTP since no domain for Resend

--- event_management/event_management/settings.py
+++ event_management/event_management/settings.py
@@ -139,9 +139,13 @@ LOGIN_URL = '/login/'
 LOGIN_REDIRECT_URL = '/dashboard/'
 LOGOUT_REDIRECT_URL = '/'
 
-EMAIL_BACKEND = 'accounts.resend_backend.ResendEmailBackend'
-RESEND_API_KEY = os.environ.get('RESEND_API_KEY', '')
-DEFAULT_FROM_EMAIL = os.environ.get('DEFAULT_FROM_EMAIL', 'EventHub <onboarding@resend.dev>')
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER', 'filaniisaac62@gmail.com')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD', '')
+DEFAULT_FROM_EMAIL = os.environ.get('DEFAULT_FROM_EMAIL', 'EventHub <filaniisaac62@gmail.com>')
 
 
 # Paystack Configuration
